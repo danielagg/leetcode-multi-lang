3fbd751142688f0a5ec87c5040c719a16fafefa0
Daniel Agg <daniel.agg@outlook.com>
2023-06-09 20:10:28 +0200

    binary search

--- typescript/index.ts
+++ typescript/index.ts
@@ -328,6 +328,25 @@ function reverseStringRecursively(s: string): string {
   return rev(s);
 }
 
+function binarySearch(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] == target) return mid;
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
 // Product of Array Except Self
 function productExceptSelf(nums: number[]): number[] {
   return [];
