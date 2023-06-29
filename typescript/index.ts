350ef9db0bddd6965d4e872b9ef893dec6a517bb
Daniel Agg <daniel.agg@outlook.com>
2023-06-29 17:32:39 +0200

    merge linked lists

--- typescript/index.ts
+++ typescript/index.ts
@@ -368,6 +368,55 @@ function reverseList(head: ListNode | null): ListNode | null {
   return newNext;
 }
 
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  if (list1 == null && list2 == null) return null;
+  if (list1 != null && list2 == null) return list1;
+  if (list1 == null && list2 != null) return list2;
+
+  let result: ListNode | null = null;
+  let resultHead: ListNode | null = null;
+
+  if (list1!.val <= list2!.val) {
+    result = list1;
+    list1 = list1!.next;
+  } else {
+    result = list2;
+    list2 = list2!.next;
+  }
+
+  resultHead = result;
+
+  while (list1 != null || list2 != null) {
+    if (list1 != null && list2 == null) {
+      result!.next = list1;
+      list1 = list1.next;
+    } else if (list1 == null && list2 != null) {
+      result!.next = list2;
+      list2 = list2.next;
+    } else {
+      if (list1!.val <= list2!.val) {
+        result!.next = list1;
+        list1 = list1!.next;
+      } else {
+        result!.next = list2;
+        list2 = list2!.next;
+      }
+    }
+
+    result = result!.next;
+  }
+
+  return resultHead;
+}
+
+const left = new ListNode(1, new ListNode(2, new ListNode(4, null)));
+const right = new ListNode(1, new ListNode(3, new ListNode(4, null)));
+
+mergeTwoLists(left, right);
+
 function searchMatrix(matrix: number[][], target: number): boolean {
   let top = 0;
   let bottom = matrix.length - 1;
