054bd9062ac1152db13645252b336313f8ecde1c
Daniel Agg <daniel.agg@outlook.com>
2023-09-05 20:27:02 +0200

    MaxDepthDfsIterative

--- csharp/04_Trees.cs
+++ csharp/04_Trees.cs
@@ -39,4 +39,28 @@ public class Trees
 
         return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
     }
+
+    public int MaxDepthDfsIterative(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        var result = 1;
+        
+        var stack = new Stack<(TreeNode node, int depth)>();
+        stack.Push((root, 1));
+
+        while (stack.Count > 0)
+        {
+            var (node, depth) = stack.Pop();
+
+            if (node != null)
+            {
+                result = Math.Max(result, depth);
+                stack.Push((node.left, depth + 1));
+                stack.Push((node.right, depth + 1));
+            }
+        }
+
+        return result;
+    }
 }
