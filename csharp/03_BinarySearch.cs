46e14ac8a5aed57c3bfdec2879d3042829bfa7ec
Daniel Agg <daniel.agg@outlook.com>
2023-10-09 19:09:30 +0200

    searchmatrix

--- csharp/03_BinarySearch.cs
+++ csharp/03_BinarySearch.cs
@@ -9,6 +9,11 @@ namespace csharp
         public void ExecuteAll()
         {
             Search(new[] { -1,0,3,5,9,12 }, 2);
+            SearchMatrix(new[] {
+                new[] {1,3,5,7},
+                new[] {10,11,16,20},
+                new[] {23,30,34,60}
+            }, 3);
         }
         
         public int Search(int[] nums, int target)
@@ -33,5 +38,45 @@ namespace csharp
 
             return -1;
         }
+
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            var topRow = 0;
+            var bottomRow = matrix.Count();
+
+            var row = 0;
+
+            while(topRow < bottomRow)
+            {
+                row = topRow + (bottomRow-topRow) / 2;
+
+                if(target > matrix[row][matrix[0].Length - 1]) {
+                    topRow = row;
+                } else if(target < matrix[row][0]) {
+                    bottomRow = row;
+                } else {
+                    break;
+                }
+            }
+
+            var low = 0;
+            var high = matrix[0].Length;
+
+            while(low < high)
+            {
+                var mid = low + (high - low) / 2;
+
+                if(matrix[row][mid] == target) {
+                    return true;
+                } else if (target > matrix[row][mid]) {
+                    low = mid;
+                } else {
+                    high = mid;
+                }
+            }
+
+            return false;
+        }
+
     }
 }
--- csharp/04_Trees.cs
+++ csharp/04_Trees.cs
@@ -33,34 +33,34 @@ public class Trees
         return root;
     }
     
-    public int MaxDepthDfsRecursive(TreeNode root)
-    {
-        if (root == null) return 0;
+    // public int MaxDepthDfsRecursive(TreeNode root)
+    // {
+    //     if (root == null) return 0;
 
-        return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
-    }
+    //     return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+    // }
 
-    public int MaxDepthDfsIterative(TreeNode root)
-    {
-        if (root == null) return 0;
+    // public int MaxDepthDfsIterative(TreeNode root)
+    // {
+    //     if (root == null) return 0;
 
-        var result = 1;
+    //     var result = 1;
         
-        var stack = new Stack<(TreeNode node, int depth)>();
-        stack.Push((root, 1));
+    //     var stack = new Stack<(TreeNode node, int depth)>();
+    //     stack.Push((root, 1));
 
-        while (stack.Count > 0)
-        {
-            var (node, depth) = stack.Pop();
+    //     while (stack.Count > 0)
+    //     {
+    //         var (node, depth) = stack.Pop();
 
-            if (node != null)
-            {
-                result = Math.Max(result, depth);
-                stack.Push((node.left, depth + 1));
-                stack.Push((node.right, depth + 1));
-            }
-        }
+    //         if (node != null)
+    //         {
+    //             result = Math.Max(result, depth);
+    //             stack.Push((node.left, depth + 1));
+    //             stack.Push((node.right, depth + 1));
+    //         }
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 }
